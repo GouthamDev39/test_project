a574cef887da2d64105e1beceb0ee3c2d0d20642
GouthamDev39 <gouthamkrishnadas@gmail.com>
2025-05-11 15:14:44 +0530

    Update settings.py
    Connected to sqlite from postgres

--- test_project/settings.py
+++ test_project/settings.py
@@ -75,15 +75,10 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_db',
-        'USER': 'batman',
-        'PASSWORD': 'pswd',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
