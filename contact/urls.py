2696434bad0f5f0744bffda461e83b562fee2389
GouthamDev39 <gouthamkrishnadas@gmail.com>
2025-05-20 21:58:59 +0530

    Adding docker file

--- contact/urls.py
+++ contact/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from .views import index, contact_view, contact_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', index, name='index'), 
     path('contact/', contact_page, name='contact_page'),  # Frontend page
     path('api/contact/', contact_view, name='contact_api'), 
