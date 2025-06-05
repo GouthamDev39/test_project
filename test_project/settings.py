d8b3ca829f57c304b9bf1564f87cfa4ac1f90a1f
GouthamDev39 <gouthamkrishnadas@gmail.com>
2025-06-05 23:00:41 +0530

    Docker/action/try3

--- test_project/settings.py
+++ test_project/settings.py
@@ -77,27 +77,27 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_db',
-        'USER': 'batman',
-        'PASSWORD': 'pswd',
-        #'HOST': 'localhost',
-        'HOST': 'postgres-db' ,#for docker
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'test_db',
+#         'USER': 'batman',
+#         'PASSWORD': 'pswd',
+#         #'HOST': 'localhost',
+#         'HOST': 'postgres-db' ,#for docker
+#         'PORT': '5432',
+#     }
+# }
 
 
 #for testing use this 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 #On Deployment
